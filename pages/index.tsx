3e117eb6b57d286d4269636f3ff9e9cd2a4bb36f
a203311 <203395950@qq.com>
2026-01-21 15:23:32 +0800

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>LÉVORA</h1>
+      <p>This is a brand new independent store.</p>
+      <a href="/product">Go to product</a>
+    </div>
+  )
+}
