599621b067d7e617a4b05b77c1f70f421bbee079
a203311 <203395950@qq.com>
2026-01-21 15:24:14 +0800

    Create index.tsx

---
+++ pages/pages/product/index.tsx
@@ -0,0 +1,9 @@
+export default function Product() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Aria Crossbody Bag</h1>
+      <p>Price: $179</p>
+      <a href="/">Back to home</a>
+    </div>
+  )
+}
